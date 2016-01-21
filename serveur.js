6123be16a776aa83eec645a579b55389c321e192
fabienbrun <fabienbrun@hotmail.fr>
2016-01-21 18:02:05 +0100

    ADD - Rest API structure

--- serveur.js
+++ serveur.js
@@ -3,87 +3,55 @@ var http = require("http");
 var fs = require("fs");
 
 var mime = require("mime"); // installer le module mime depuis la console à la racine du projet , avec "npm install mime"
+var express = require("express"); // installer le module connect depuis la console à la racine du projet , avec "npm install connect"
 
-var connect = require("connect"); // installer le module connect depuis la console à la racine du projet , avec "npm install connect"
+// midldlewares
+var logger = require('morgan');
+var favicon = require('serve-favicon');
 
-var serveStatic = require("serveStatic"); // Middlewere sous forme de module static  connect serveStatic  : "npm install serve-static"
+
+var serveStatic = require("serve-static"); // Middlewere sous forme de module static  connect serveStatic  : "npm install serve-static"
 
 var PORT = 8080;
 
-
-var envoieFichier = function(res, url ) {
-    console.log("envoie fichier :" + url);
-
-    var path = __dirname + "/" + url;
-    fs.stat(path, function(err, stats){
-        if(!err && stats.isFile() ) {
-
-            var flux = fs.createReadStream(path, {
-                   flags: "r",
-                    autoClose: true
-                });
-
-            var typeMime = mime.lookup(path);
-
-            res.writeHead(200, {"Content-Type": typeMime})
-            flux.pipe(res);
-        } else {
-            envoie404(res);
-        }
-    })
+var app = express();
 
 
-}
-
-var envoie404 = function(res) {
-    res.writeHead(404, {"Content-Type": "text/html"});
-    res.end("<h1>Page introuvable</h1>");
-}
-
-
-var app = connect();
+app.use(favicon(__dirname + "/favicon.ico"));
+app.use(logger(":method :url"));
+app.use(serveStatic(__dirname + "/"));
 
 
 
-app.use(function(req,res, next){
-    if(req.url == "/") {
-        res.writeHead(301, {"Location": "index.html"});
-        res.end();
-    } else {
-     next();
-    }
+// API
+
+// Récupérer la liste des dossiers
+// GET /api/dossiers
+app.get("/api/dossiers", function(req, res) {
+    // ...
 });
 
-
-app.use(function(req,res){
-        envoieFichier(res,req.url);
+// Récupérer un dossier
+// GET /api/dossiers/idDossier
+app.get("/api/dossiers/:idDossier", function(req, res) {
+    // ...
 });
 
+// Récupérer un mail
+// GET /api/dossier/idDossier/idMail
+app.get("/api/dossier/:idDossier/:idMail", function(req, res) {
+    // ...
+});
 
+// Envoyer un mail
+// POST /api/envoi
+app.post("/api/envoi", function(req, res) {
+    // ...
+});
 
 
 
 http.createServer(app).listen(PORT);
-
-
-
-
-
-
-
-/*http.createServer(function(req, res) {
-
-    if(req.url == "/") {
-        res.writeHead(301, {"Location": "index.html"});
-        res.end();
-    }
-    else  {
-        envoieFichier(res,req.url);
-    }
-
-
-
-}).listen(PORT); */
 
 console.log("Serveur démaré sur le port :" + PORT);
 
