d0cce835eb1d9b77c306ff475d6d76bd44c198f0
fabienbrun <fabienbrun@hotmail.fr>
2016-01-22 11:07:27 +0100

    Add - API Webservices Rest

--- serveur.js
+++ serveur.js
@@ -8,6 +8,7 @@ var express = require("express"); // installer le module connect depuis la conso
 // midldlewares
 var logger = require('morgan');
 var favicon = require('serve-favicon');
+var bodyParser = require("body-parser");
 
 
 var serveStatic = require("serve-static"); // Middlewere sous forme de module static  connect serveStatic  : "npm install serve-static"
@@ -24,31 +25,48 @@ app.use(serveStatic(__dirname + "/"));
 
 
 // API
+var api = express();
 
 // Récupérer la liste des dossiers
 // GET /api/dossiers
-app.get("/api/dossiers", function(req, res) {
-    // ...
+api.get("/dossiers", function(req, res) {
+    res.send([
+        { value: "RECEPTION", label: 'Boite de reception'},
+        { value: "ARCHIVES", label: 'Archives'},
+        { value: "ENVOYES", label: 'Envoyés'},
+        { value: "SPAM", label: 'Courrier indésirable'}
+    ]);
 });
 
 // Récupérer un dossier
 // GET /api/dossiers/idDossier
-app.get("/api/dossiers/:idDossier", function(req, res) {
-    // ...
+api.get("/dossiers/:idDossier", function(req, res) {
+    res.send([
+        { id: 1, from: "Albator", to: "Rudy", subject: "Je reviens", date: new Date(2014, 2, 20, 15, 30), content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis." },
+        { id: 2, from: "Capitaine Flam", to: "Rudy", subject: "Bisous de l'espace", date: new Date(2014, 3, 18, 16, 12), content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>." },
+        { id: 3, from: "Pikachu", to: "Rudy", subject: "Pika pika !", date: new Date(2014, 2, 15, 16, 12), content: "Pika pika ! Chuuuuuu. Pika pika ! Chuuuuuu. Pika pika ! Chuuuuuu. Pika pika ! Pika pika ? Piiiiika Chuuuuuu. Pika pika ! Pikachu. Pika pika pika." },
+        { id: 4, from: "Barbapapa", to: "Rudy", subject: "Hulahup Barbatruc", date: new Date(2014, 2, 15, 14, 15), content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>." }
+    ]);
 });
 
 // Récupérer un mail
 // GET /api/dossier/idDossier/idMail
-app.get("/api/dossier/:idDossier/:idMail", function(req, res) {
-    // ...
+api.get("/dossiers/:idDossier/:idMail", function(req, res) {
+    res.send({ id: 1, from: "Albator", to: "Rudy", subject: "Je reviens", date: new Date(2014, 2, 20, 15, 30), content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis." });
 });
+
+
+
+app.use(bodyParser.json());
 
 // Envoyer un mail
 // POST /api/envoi
-app.post("/api/envoi", function(req, res) {
-    // ...
+api.post("/envoi", function(req, res) {
+    res.send({ succes: true, email: req.body });
 });
 
+
+app.use("/api", api);
 
 
 http.createServer(app).listen(PORT);
